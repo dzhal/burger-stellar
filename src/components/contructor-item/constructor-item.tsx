57ce274bc44c9539912b9eeb5b7dae982143e4d3
dzhal <djal.amninov@gmail.com>
2022-04-06 22:14:19 +0300

    fixed propstypes and removed useful dependensies

--- src/components/contructor-item/constructor-item.tsx
+++ src/components/contructor-item/constructor-item.tsx
@@ -91,23 +91,21 @@ function ConstructorItem({ item, id, index, removeHandler }: ElementProps) {
 }
 
 ConstructorItem.propTypes = {
-  item: PropTypes.arrayOf(
-    PropTypes.shape({
-      _id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      type: PropTypes.string.isRequired,
-      proteins: PropTypes.number.isRequired,
-      fat: PropTypes.number.isRequired,
-      carbohydrates: PropTypes.number.isRequired,
-      calories: PropTypes.number.isRequired,
-      price: PropTypes.number.isRequired,
-      image: PropTypes.string.isRequired,
-      image_mobile: PropTypes.string.isRequired,
-      image_large: PropTypes.string.isRequired,
-      __v: PropTypes.number.isRequired,
-      uuid: PropTypes.string,
-    })
-  ).isRequired,
+  item: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    proteins: PropTypes.number.isRequired,
+    fat: PropTypes.number.isRequired,
+    carbohydrates: PropTypes.number.isRequired,
+    calories: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    image_mobile: PropTypes.string.isRequired,
+    image_large: PropTypes.string.isRequired,
+    __v: PropTypes.number.isRequired,
+    uuid: PropTypes.string,
+  }),
   id: PropTypes.string.isRequired,
   index: PropTypes.number.isRequired,
   removeHandler: PropTypes.func.isRequired,
